9953d94892f501bddce165168a5d330b742f5ca4
yuxxeun <dfd50dfd553fd7ef36cc@email.privaterelay.co>
2023-03-10 14:43:23 +0700

    Initial commit

---
+++ app/View/Components/GuestLayout.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class GuestLayout extends Component
+{
+    public $title;
+
+    public function __construct($title = null)
+    {
+        $this->title = $title;
+    }
+
+    public function render(): View
+    {
+        return view('layouts.guest');
+    }
+}
